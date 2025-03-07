5749297d530e26dfc4b56e5dedfbcbbc70b3de88
nernass <nrmen-2011@hotmail.com>
2025-03-07 16:48:16 +0300

    Delete test/java/com/springboot/springbootsecurity directory

--- test/java/com/springboot/springbootsecurity/main/java/com/springboot/springbootsecurity/user/service/UserLogoutService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.springbootsecurity.user.service;
-
-import com.springboot.springbootsecurity.auth.model.dto.request.TokenInvalidateRequest;
-
-public interface UserLogoutService {
-
-    void logout(final TokenInvalidateRequest tokenInvalidateRequest);
-
-}
